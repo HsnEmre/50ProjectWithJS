a5ce4fed6591f6c1985774de221f7dbc53bea969
Hasan Emre Buzkan <92093161+HsnEmre@users.noreply.github.com>
2024-08-20 16:31:33 +0300

    Add files via upload

---
+++ 12-)Faq Collapse/script.js
@@ -0,0 +1,7 @@
+const toggles=document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggles=>{
+    toggle.addEventListener('click',()=>{
+        tooggle.parentNode.classList.toggle('active')
+    })
+})
